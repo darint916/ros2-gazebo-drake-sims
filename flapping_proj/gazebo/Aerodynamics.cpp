2cd93c010dd4c06fcead99656fc34c1039a6496a
Darin Tsai <darint916@gmail.com>
2023-06-16 18:11:20 -0400

    Plugin Outline Setup Todos added, Predefined system req added as preupdate + configure Need to load all parameters and decide on setup Update needs to be done - all aero forces and data pub

--- flapping_proj/gazebo/Aerodynamics.cpp
+++ flapping_proj/gazebo/Aerodynamics.cpp
@@ -1,11 +1,94 @@
-class Aerodynmaics 
+#include <mutex>
+#include <ignition/plugin/Register.hh>
+#include <ignition/transport/Node.hh>
+#include <ignition/common/Profiler.hh>
+#include <ignition/math/Quaternion.hh>
+#include "ignition/gazebo/components/JointPosition.hh"
+#include "ignition/gazebo/components/Pose.hh" 
+#include "ignition/gazebo/components/Name.hh" 
+#include "ignition/gazebo/Link.hh"
+#include "ignition/gazebo/Model.hh"
+
+#include "Aerodynamics.hpp"
+
+/*
+    TODO Considerations:
+    Decide whether to use one plugin per link or one plugin for all
+    One plugin per link = separate data publication, (could work), need to catch separate topics for data retreival
+    One plugin for all = one data publication, data formatting different, prob Pose_V
+    Need to implement type matching for aerodynamics equation
+    Have another struct type for holding params/info for each link
+    Have default equation multiplied by modifiers like cos, default 1
+    Fix header vars, no need for mutex? 
+    
+    TODO next:
+    Test SDF get element for child query http://osrf-distributions.s3.amazonaws.com/sdformat/api/3.0/classsdf_1_1Element.html#a5af1be8776ec7f70edc85204e1f76de7
+    Test diff Drive coordinate publication info, what it should publish like, worldPose vs Pose for components.
+    Fix Parameter input
+    Calculate wrench values from aerodynamics equation
+    Publish wrench values
+*/
+
+
+using namespace aerodynamics;
+namespace igz = ignition::gazebo;
+Aerodynamics::Aerodynamics() {
+    this->dataPtr = std::make_unique<AerodynamicsData>();
+}
+
+void Aerodynamics::Configure(const Entity &_entity, const std::shared_ptr<const sdf::Element> &_sdf, EntityComponentManager &_ecm, EventManager &)
 {
-    void OdometryState::Configure(const igz::Entity &_entity, const std::shared_ptr<const sdf::Element> &_sdf, igz::EntityComponentManager &_ecm, igz::EventManager &/*_eventMgr*/)
-    {
-        this->dataPtr->model = igz::Model(_entity);
-        if(!this->dataPtr->model.Valid(_ecm)) {
-            ignerr << "plugin should be attached to a model entity, fail init" << std::endl;
-            return;
+    this->dataPtr->model = Model(_entity);
+    if(!this->dataPtr->model.Valid(_ecm)) {
+        ignerr << "plugin should be attached to a model entity, fail init" << std::endl;
+        return;
+    }
+    // _ecm.Component<ignition::gazebo::components::Pose>(
+    this->dataPtr->sdfConfig = _sdf->Clone();
+}
+
+void Aerodynamics::PreUpdate(const UpdateInfo &_info, EntityComponentManager &_ecm)
+{ //could slow down as preupdate always called at each timestep?
+    IGN_PROFILE("Aerodynamics::PreUpdate");
+    if (!this->dataPtr->initialized){
+        this->dataPtr->initialized = true;
+       
+        this->dataPtr->Load(_ecm, this->dataPtr->sdfConfig);
+        if (this->dataPtr->validConfig){
+            igz::Link link(this->dataPtr->linkEntity);
+            link.EnableVelocityChecks(_ecm, true);
         }
     }
+
+    if (_info.paused) return;
+
+    if (this->dataPtr->initialized && this->dataPtr->validConfig){
+        this->dataPtr->Update(_ecm);
+    }
 }
+
+void AerodynamicsData::Load(const EntityComponentManager &_ecm, const sdf::ElementPtr &_sdf)
+{
+    //TODO: Finish Data query from SDF
+    std::string linkName = _sdf->Get<std::string>("link_name", "");
+    if (linkName.empty())
+    {
+        ignerr << "Aerodynamics plugin should have a <link_name> child." << std::endl;
+        this->validConfig = false;
+        return;
+    }
+
+    this->linkEntity = this->model.LinkByName(_ecm, linkName);
+    this->linkPose = this->model.Pose(_ecm, this->linkEntity);
+}
+
+void AerodynamicsData::Update(EntityComponentManager &_ecm)
+{
+    //Todo calculate aerodynamic equations, query link pose, apply wrench, publish wrench
+}
+IGNITION_ADD_PLUGIN(Aerodynamics,
+                    System,
+                    Aerodynamics::ISystemConfigure,
+                    Aerodynamics::ISystemPreUpdate)
+
+IGNITION_ADD_PLUGIN_ALIAS(LiftDrag, "gz::sim::systems::LiftDrag")
--- flapping_proj/gazebo/Aerodynamics.hpp
+++ flapping_proj/gazebo/Aerodynamics.hpp
@@ -6,6 +6,54 @@
 
 namespace aerodynamics 
 {
+    namespace igz = ignition::gazebo;
+    /*
+        SDF Parameter Shape
+        <plugin filename="aerodynamics_plugin" name="aerodynamics">
+            
+    */
+    struct AerodynamicsData
+    {
+        igz::Model model{igz::kNullEntity};
+        ignition::transport::Node node;
+        ignition::transport::Node::Publisher aerodynamicsPublisher;
+
+        std::chrono::steady_clock::duration aerodynamicsPubPeriod;
+        std::chrono::steady_clock::duration prevAerodynamicsPubTime = std::chrono::steady_clock::duration::zero();
+        std::mutex mutex;
+
+        std::string modelName;
+        std::string linkType; //Wing, Generic, etc.
+
+        double stallAngle = 0; //degrees
+
+        bool reportData = false;
+        bool validConfig = false;
+        bool initialized = false;
+        sdf::ElementPtr sdfConfig;
+
+        //Sim parameters
+        ignition::msgs::Pose linkPose;
+        
+
+        //sdf parameters
+        double fluidDensity = 1.293; //kg*m^-3 air density at 273K
+        double dragCoefficient = 1; //unitless + variable
+        double liftCoefficient = 1; //unitless + variable
+
+        void Load(const EntityComponentManager &_ecm, const sdf::ElementPtr &_sdf);
+        void Update(EntityComponentManager &_ecm);
+    }
     
+    class Aerodynamics : public igz::System, public igz::ISystemConfigure, igz:: ISystemPostUpdate
+    {
+        private:
+            std::unique_ptr<AerodynamicsData> dataPtr;
+        public:
+            Aerodynamics();
+            ~Aerodynamics() override = default;
+            void Configure(const igz::Entity &_entity, const std::shared_ptr<const sdf::Element> &_sdf, igz::EntityComponentManager &_ecm, igz::EventManager &_eventMgr) override;
+            void PostUpdate(const igz::UpdateInfo &_info, const igz::EntityComponentManager &_ecm) override;
+    };
 }
 #endif // __AERODYNAMICS_HPP__
