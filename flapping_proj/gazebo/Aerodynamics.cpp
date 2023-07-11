8a90cac6e8530ad090a682ad8439447841aef915
Darin Tsai <darint916@gmail.com>
2023-07-11 19:25:01 -0400

    cleanup

--- flapping_proj/gazebo/Aerodynamics.cpp
+++ flapping_proj/gazebo/Aerodynamics.cpp
@@ -23,48 +23,33 @@
 #include "ignition/gazebo/Model.hh"
 #include "ignition/gazebo/Util.hh"
 
-
-
 #include "Aerodynamics.hpp"
 
 /*
-    TODO Considerations:
-    Decide whether to use one plugin per link or one plugin for all
-    One plugin per link = separate data publication, (could work), need to catch separate topics for data retreival
-    One plugin for all = one data publication, data formatting different, prob Pose_V
-    Need to implement type matching for aerodynamics equation
-    Have another struct type for holding params/info for each link
-    Have default equation multiplied by modifiers like cos, default 1
-    Fix header vars, no need for mutex? 
-    
     TODO next:
-    Test SDF get element for child query http://osrf-distributions.s3.amazonaws.com/sdformat/api/3.0/classsdf_1_1Element.html#a5af1be8776ec7f70edc85204e1f76de7
-    Test diff Drive coordinate publication info, what it should publish like, worldPose vs Pose for components.
-    Fix Parameter input
-    Calculate wrench values from aerodynamics equation
-    Publish wrench values
+    Data publishing onto topic, gui to reflect that data
+    Implement angle of attack formulation
+    Change stroke angle 
+    Generic link implementation needed
+    Control joint + wing pitch joint implementation needed ?? not used as of now
+    TODO DATA:
+    one topic per component
+    on topic, pose3d arrays, velocity calculated.. such as:
 */
 
 using namespace std;
 using namespace aerodynamics;
 namespace igz = ignition::gazebo;
 
-
 //Working func, tested, comma delimiter, could be optimized? (string copied)
 static std::vector<double> ParseStringStringToVector(std::stringstream &ss) 
 {
     std::vector<double> vectorList; 
-    // std::vector<std::string> tokens;
     std::string token;
     while(std::getline(ss, token, ',')){
         token.erase(std::remove(token.begin(), token.end(), ' '), token.end()); //remove spaces
-        // tokens.push_back(token);
         try { //string to double parsing
-            // if(tokens.size() % 3 == 0) {
-                // ignition::math::Vector3d vector(std::stod(tokens[0]), std::stod(tokens[1]), std::stod(tokens[2]));
             vectorList.push_back(stod(token));
-                // tokens.clear();
-            // }
         } catch (const std::invalid_argument& ia) {
             ignerr << "STOD Invalid argument: " << ia.what() << std::endl;
             return vectorList;
@@ -87,28 +72,27 @@ void Aerodynamics::Configure(const igz::Entity &_entity, const std::shared_ptr<c
         ignerr << "plugin should be attached to a model entity, fail init" << std::endl;
         return;
     }
-    // _ecm.Component<ignition::gazebo::components::Pose>(
     this->dataPtr->sdfConfig = _sdf->Clone();
 }
 
 void Aerodynamics::PreUpdate(const igz::UpdateInfo &_info, igz::EntityComponentManager &_ecm)
 { //could slow down as preupdate always called at each timestep?
     IGN_PROFILE("Aerodynamics::PreUpdate");
-    // ignerr << "PreUpdate" << std::endl;
+    // We call Load here instead of Configure because we can't be guaranteed
+    // that all entities have been created when Configure is called
+    //(Should test ^)
     if (!this->dataPtr->initialized){
         this->dataPtr->initialized = true;
-       
         this->dataPtr->Load(_ecm, this->dataPtr->sdfConfig);
-        
     }
 
     if (_info.paused) return;
+    
     if(!this->dataPtr->validConfig){
         ignerr << "Invalid config" << std::endl;
         return;
     }
     if (this->dataPtr->initialized && this->dataPtr->validConfig){
-        // ignerr << "Valid config" << std::endl;
         this->dataPtr->Update(_ecm);
     }
     return;
@@ -116,14 +100,9 @@ void Aerodynamics::PreUpdate(const igz::UpdateInfo &_info, igz::EntityComponentM
 
 void AerodynamicsData::Load(igz::EntityComponentManager &_ecm, const sdf::ElementPtr &_sdf)
 {
-    //TODO: Add checking for link + joint is actually a link + joint (not that important) check ref for other checks
-    // MAKE AOA list?
     auto sdf_ptr = const_cast<sdf::Element*>(_sdf.get());
     sdf::ElementPtr linkElement = sdf_ptr->GetElement("link");
     while(linkElement != nullptr){
-        //if below doesnt work, try
-        //std::string linkName = linkElement->Get<std::string>("link_name", "").first;
-        //std::string linkType = linkElement->Get<std::string>("link_type", "").first;
         std::string linkName = linkElement->GetElement("link_name")->Get<std::string>();
         std::string linkType = linkElement->GetElement("link_type")->Get<std::string>();
         if (linkName.empty() || linkType.empty())
@@ -153,15 +132,15 @@ void AerodynamicsData::Load(igz::EntityComponentManager &_ecm, const sdf::Elemen
         this->linkMap[linkName].liftCoefficient = linkElement->GetElement("lift_coefficient")->Get<double>();
         if(linkType == "wing") {
             this->linkMap[linkName].wingParameters.blades = linkElement->GetElement("blades")->Get<int>();
-            // this->linkMap[linkName].wingParameters.wingSpan = linkElement->GetElement("wing_span")->Get<double>();
+
             std::string controlJointName = linkElement->GetElement("control_joint")->Get<std::string>();
-            //NEED TO DO JOINT NAME VERIFICATION???
             igz::Entity jointEntity = this->model.JointByName(_ecm, controlJointName);
             if (jointEntity == igz::kNullEntity) {
                 ignerr << "Control Joint [" << controlJointName << "] not found" << std::endl;
                 this->validConfig = false;
             }
             this->linkMap[linkName].wingParameters.controlJointEntity = jointEntity;
+
             std::string wingPitchJointName = linkElement->GetElement("wing_pitch_joint")->Get<std::string>();
             this->linkMap[linkName].wingParameters.wingPitchJointEntity = this->model.JointByName(_ecm, wingPitchJointName);
             if (this->linkMap[linkName].wingParameters.wingPitchJointEntity == igz::kNullEntity) {
@@ -171,31 +150,8 @@ void AerodynamicsData::Load(igz::EntityComponentManager &_ecm, const sdf::Elemen
             //Creating component will create error, might have to do the link properties
             // _ecm.CreateComponent(this->linkMap[linkName].linkEntity, igz::components::WorldPose()); //Init pose values for query
             _ecm.CreateComponent(jointEntity, igz::components::JointVelocity()); //Init vel values for query
-            //String to values for blade chord list
-            // _ecm.CreateComponent()
-            // std::stringstream ss(linkElement->GetElement("blade_chord_list")->Get<std::string>());
-            // std::string token;
-            // while(std::getline(ss, token, ',')){
-            //     token.erase(std::remove(token.begin(), token.end(), ' '), token.end());
-            //     try { //string to double parsing
-            //         this->linkMap[linkName].wingParameters.bladeChordList.push_back(std::stod(token));
-            //     } catch (const std::invalid_argument& ia) {
-            //         ignerr << "blade_chord_list STOD Invalid argument: " << ia.what() << std::endl;
-            //         this->validConfig = false;
-            //         return;
-            //     } catch (const std::out_of_range& oor) {
-            //         ignerr << "blade_chord_list STOD Out of Range error: " << oor.what() << std::endl;
-            //         this->validConfig = false;
-            //         return;
-            //     }
-            // }
-            // if (this->linkMap[linkName].wingParameters.bladeChordList.size() != this->linkMap[linkName].wingParameters.blades){
-            //     ignerr << "blade_chord_list size does not match blades" << std::endl;
-            //     this->validConfig = false;
-            //     return;
-            // }
-
-            //String to values for center pressure list
+            
+            //Parse in center pressure list into vector of vectors
             std::stringstream ss2(linkElement->GetElement("center_pressure_list")->Get<std::string>());
             std::vector<double> parsedList = ParseStringStringToVector(ss2);
             for(int i = 0; i < parsedList.size(); i+= 3){
@@ -207,6 +163,7 @@ void AerodynamicsData::Load(igz::EntityComponentManager &_ecm, const sdf::Elemen
                 return;
             }
 
+            //Parse in up vector list into vector of vectors
             std::stringstream ss3(linkElement->GetElement("upward_vector_list")->Get<std::string>());
             parsedList = ParseStringStringToVector(ss3);
             for(int i = 0; i < parsedList.size(); i+= 3){
@@ -219,6 +176,7 @@ void AerodynamicsData::Load(igz::EntityComponentManager &_ecm, const sdf::Elemen
                 return;
             }
          
+            //Parse in blade area list into vector of vectors
             std::stringstream ss4(linkElement->GetElement("blade_area_list")->Get<std::string>());
             this->linkMap[linkName].wingParameters.bladeAreaList = ParseStringStringToVector(ss4);
             if (this->linkMap[linkName].wingParameters.bladeAreaList.size() != this->linkMap[linkName].wingParameters.blades){
@@ -233,8 +191,6 @@ void AerodynamicsData::Load(igz::EntityComponentManager &_ecm, const sdf::Elemen
     }
     this->validConfig = true;
 }
-
-
 
 
 void AerodynamicsData::Update(igz::EntityComponentManager &_ecm)
