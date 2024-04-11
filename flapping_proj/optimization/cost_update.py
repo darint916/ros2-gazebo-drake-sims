ec95aa69f7e17aa02726457d2e3b37539dfe30dc
Darin Tsai <darint916@gmail.com>
2024-04-11 15:29:32 -0400

    Motor torque map Ros2, bug fix, todo and issues added to readme

--- flapping_proj/optimization/cost_update.py
+++ flapping_proj/optimization/cost_update.py
@@ -7,6 +7,8 @@
 
 # u = r_a * i_a + k_a * omega, k_a * i_a = torque where u is input voltage, r_a is motor resistance, i_a is motor current, k_a is the torque constant of the motor, and omega is the rotational velocity of the motor
 # torque = (u - k_a * omega) * k_a / r_a
+#r_a = motor_resistance 
+#k_a = motor_torque_constant
 # power is just torque * omega
 # tbh just apply a sinusoidal torque for now, and can calculate power from input and measured omega
 # for instantanious power, need the root mean square average over the last .2 seconds, for lift need the straight average over the last .2 seconds
