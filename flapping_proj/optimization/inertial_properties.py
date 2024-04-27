94f9146f35e1376e7be70566c801a2309c705c6d
BrightKnight <kyled2002@hotmail.com>
2024-04-27 15:13:05 -0400

    Wing Refactor Instead of storing curves, wings are now objects that contain points that define the trailing edge curve. The inertial properties of the wing are now stored in this object and calculated on initialization rather than needing more function calls. Refactored inertial_properties to use general functions to calculate moments of inertia of different curves seperately rather than one predefined set. This allows for easier expansion of wing classes.

--- flapping_proj/optimization/inertial_properties.py
+++ flapping_proj/optimization/inertial_properties.py
@@ -14,53 +14,74 @@ rho_h = np.pi / 4 * D_H * 1854.55 #kg/m
 #area density for 12 micron PET sheet
 rho_mem = 0.05 * 0.0005 / 0.00254**2 / 2.2 #kg/m**2
 
-class BezierCurve():
-    y0 = None
-    z0 = None
-    y1 = None
-    z1 = None
-    y2 = None
-    z2 = None
-    y3 = None
-    z3 = None
-    
-    def B_y(self, t):
-        return (1 - t)**3 * self.y0 + 3*(1 - t)**2 * t * self.y1 + 3*(1 - t) * t**2 * self.y2 + t**3  * self.y3
-    
-    def B_z(self, t):
-        return (1 - t)**3 * self.z0 + 3*(1 - t)**2 * t * self.z1 + 3*(1 - t) * t**2 * self.z2 + t**3  * self.z3
-    
-    def dB_y(self, t):
-        return 3 * (1 - t)**2 * (self.y1 - self.y0) + 6 * (1 - t) * t * (self.y2 - self.y1) + 3 * t**2 * (self.y3 - self.y2)
-    
-    def dB_z(self, t):
-        return 3 * (1 - t)**2 * (self.z1 - self.z0) + 6 * (1 - t) * t * (self.z2 - self.z1) + 3 * t**2 * (self.z3 - self.z2)
-    
-    def __init__(self, y_0, z_0, y_1, z_1, y_2, z_2, y_3, z_3):
-        self.y0 = y_0
-        self.z0 = z_0
-        self.y1 = y_1
-        self.z1 = z_1
-        self.y2 = y_2
-        self.z2 = z_2
-        self.y3 = y_3
-        self.z3 = z_3
+#calculates moments of inertia for a slender rod in the yz plane
+def line_I(y0, z0, y1, z1, diameter = D_LE) -> np.array:
+    rho = np.pi / 4 * diameter**2 * 1854.55 #kg/m
+    del_y = y1 - y0
+    del_z = z1 - z0
 
-def wing_I(curve, debug = False) -> np.array:
+    line_length = np.sqrt(del_y**2 + del_z**2)
+
+    y_integrated = del_y**2 / 3 + del_y * y0 + y0**2
+    z_integrated = del_z**2 / 3 + del_z * z0 + z0**2
+
+    I = np.array([y_integrated + z_integrated, 
+                  z_integrated, 
+                  line_length * y_integrated, 
+                  0, 
+                  0, 
+                  (del_y*del_z/3 + (del_z*y0 + del_y*z0)/2 + y0*z0)]) * rho * line_length
+    return I
+
+#calculates the mass of a rod in the yz plane
+def line_m(y0, z0, y1, z1, diameter = D_LE) -> float:
+    rho = np.pi / 4 * diameter**2 * 1854.55 #kg/m
+    del_y = y1 - y0
+    del_z = z1 - z0
+
+    line_length = np.sqrt(del_y**2 + del_z**2)
+    return rho * line_length
+
+
+#calculates the moments of inertia for a slender rod in the yz plane that follows a curve
+def curve_I(curve, diameter = D_TE, t_max = 1) -> np.array:
+    rho = np.pi / 4 * diameter**2 * 1854.55 #kg/m
+    I = np.array([sc.integrate.quad(lambda t: (curve.y(t)**2 + curve.z(t)**2) * dm_curve(curve, t, rho), 0, t_max)[0],
+                  sc.integrate.quad(lambda t: (curve.z(t)**2) * dm_curve(curve, t, rho), 0, t_max)[0],
+                  sc.integrate.quad(lambda t: (curve.y(t)**2) * dm_curve(curve, t, rho), 0, t_max)[0],
+                  0,
+                  0,
+                  sc.integrate.quad(lambda t: (curve.y(t) * curve.z(t)) *dm_curve(t), 0, t_max)[0]])
+     
+    return I
+
+#calculates the mass of a rod following a curve in the yz plane
+def curve_m(curve, diameter = D_TE, t_max = 1) -> float:
+    rho = np.pi / 4 * diameter**2 * 1854.55 #kg/m
+    return sc.integrate.quad(lambda t: dm_curve(curve, t, rho), 0, t_max)[0]
+     
+
+#calculates the moment of inertia for a planar film bounded by a curve in the yz plane.
+#Assumes the curve is below the y axis
+def film_I(curve, rho = rho_mem) -> np.array:
+    I = np.array([-rho * sc.integrate.quad(lambda t: (curve.z(t)*curve.y(t)**2 + 1/3 * curve.z(t)**3) * curve.dy(t), 0, 1)[0],
+                  -1/3 * rho * sc.integrate.quad(lambda t: curve.z(t)**3 * curve.dy(t), 0, 1)[0],
+                  -rho * sc.integrate.quad(lambda t: curve.y(t)**2 * curve.z(t) * curve.dy(t), 0, 1)[0],
+                   0,
+                   0,
+                  -0.5 * rho * sc.integrate.quad(lambda t: curve.y(t) * curve.z(t)**2 * curve.dy(t), 0, 1)[0]])
+    return I
+
+#calculates the mass of a planar film bounded by a curve in the yz plane.
+#Assumes the curve is below the y axis
+def film_m(curve, rho = rho_mem) -> float:
+    return rho * sc.integrate.quad(lambda t: np.abs(curve.z(t) * curve.dy(t)), 0, 1)[0]
+
+def bez_wing_I(curve, d_le = D_LE, d_h = D_H, d_te = D_TE, debug = False) -> np.array:
     #leading edge
-    I_LE = np.array([1/3 * rho_le * curve.y3 * (3 * (D_LE / 2)**2 + curve.y3**2), 
-                     .5 * rho_le * (D_LE / 2)**3, 
-                     1/3 * rho_le * curve.y3 * (3 * (D_LE / 2)**2 + curve.y3**2), 
-                     0, 
-                     0, 
-                     0])
+    I_LE = line_I(0, 0, curve.y3, 0, diameter=d_le)
     #hinge
-    I_H = np.array([rho_h * (1/3 * curve.y3**3 + curve.z0**2 * curve.y3 - (1/3 * (curve.y0 + D_TE + D_H)**3 + curve.z0**2 * (curve.y0  + D_TE + D_H))), 
-                    rho_h * curve.z0**2 * (curve.y3 - (curve.y0 + D_TE + D_H)), 
-                    1/3 * rho_h * (curve.y3**3 - (curve.y0 + D_TE + D_H)**3),
-                    0, 
-                    0, 
-                    0.5 * rho_h*curve.z0*(curve.y3 - (curve.y0  + D_TE + D_H))])
+    I_H = line_I(curve.y0, curve.z0, curve.y3, curve.z3, diameter=d_h)
     
     t1 = 2 * curve.z1 - curve.z0 - curve.z2 
 
@@ -71,20 +92,10 @@ def wing_I(curve, debug = False) -> np.array:
     t_max = 0.5 if abs(t3) < 1e-6 else t1 + np.sqrt(t2) / t3
 
     #trailing edge
-    I_TE = np.array([sc.integrate.quad(lambda t: (curve.B_y(t)**2 + curve.B_z(t)**2) * dm_te(t), 0, t_max)[0],
-                     sc.integrate.quad(lambda t: (curve.B_z(t)**2) * dm_te(t), 0, t_max)[0],
-                     sc.integrate.quad(lambda t: (curve.B_y(t)**2) * dm_te(t), 0, t_max)[0],
-                     0,
-                     0,
-                     sc.integrate.quad(lambda t: (curve.B_y(t) * curve.B_z(t)) * dm_te(t), 0, t_max)[0]])
+    I_TE = curve_I(curve, t_max = t_max, diameter=d_te)
     
     #membrane
-    I_film = np.array([-rho_mem * sc.integrate.quad(lambda t: (curve.B_z(t)*curve.B_y(t)**2 + 1/3 * curve.B_z(t)**3) * curve.dB_y(t), 0, 1)[0],
-                       -1/3 * rho_mem * sc.integrate.quad(lambda t: curve.B_z(t)**3 * curve.dB_y(t), 0, 1)[0],
-                       -rho_mem * sc.integrate.quad(lambda t: curve.B_y(t)**2 * curve.B_z(t) * curve.dB_y(t), 0, 1)[0],
-                       0,
-                       0,
-                       -0.5 * rho_mem * sc.integrate.quad(lambda t: curve.B_y(t) * curve.B_z(t)**2 * curve.dB_y(t), 0, 1)[0]])
+    I_film = film_I(curve)
     
     if debug:
         print(I_LE)
@@ -94,13 +105,16 @@ def wing_I(curve, debug = False) -> np.array:
 
     return I_LE + I_H + I_TE + I_film
     
-def dm_te(curve, t):
-        return rho_te * np.sqrt(curve.dB_z(t)**2 + curve.dB_y(t)**2)
+def dm_curve(curve, t, rho):
+        return rho * np.sqrt(curve.dz(t)**2 + curve.dy(t)**2)
 
-def wing_mass(curve) -> float:
+def bez_wing_mass(curve, d_le = D_LE, d_h = D_H, d_te = D_TE) -> float:
+    rho_le = np.pi / 4 * d_le**2 * 1854.55 #kg/m
+    rho_te = np.pi / 4 * d_te**2 * 1854.55 #kg/m
+    rho_h = np.pi / 4 * d_h * 1854.55 #kg/m
     m_LE = curve.y3 * rho_le
-    m_h = (curve.y3 - (curve.y0 + D_H + D_TE)) * rho_h
-    m_film = sc.integrate.quad(lambda t: curve.B_z(t) * curve.dB_y(t), 0, 1)
+    m_h = (curve.y3 - (curve.y0 + d_h + d_te)) * rho_h
+    m_film = sc.integrate.quad(lambda t: -curve.z(t) * curve.dy(t), 0, 1)
 
     t1 = 2 * curve.z1 - curve.z0 - curve.z2 
     t2 = curve.z1**2 - curve.z1 * curve.z2 - curve.z1 * curve.z3 + curve.z0 * curve.z3 + curve.z2**2 - curve.z0 * curve.z2 
@@ -108,6 +122,6 @@ def wing_mass(curve) -> float:
 
     t_max = 0.5 if abs(t3) < 1e-6 else t1 + np.sqrt(t2) / t3
 
-    m_te = sc.integrate.quad(lambda t: dm_te(curve, t), 0, t_max)
+    m_te = sc.integrate.quad(lambda t: dm_curve(curve, t, rho_te), 0, t_max)
 
     return m_LE + m_h + m_film + m_te
---
+++ flapping_proj/optimization/wing_classes.py
@@ -0,0 +1,102 @@
+from inertial_properties import*
+import numpy as np
+#wing with a trailing edge shaped like a bezier curve.
+#there is the leading edge, a hinge bar, and trailing edge connected by a film 
+
+class BezierWing():
+    y0 = None
+    z0 = None
+    y1 = None
+    z1 = None
+    y2 = None
+    z2 = None
+    y3 = None
+    z3 = None
+
+    D_LE = 0.001 #m
+    D_TE = 0.001 #m
+    D_H  = 0.00025 #m
+
+    #1d array of [Ixx, Iyy, Izz, Ixy, Ixz, Iyz]
+    I = None
+
+    #Wing mass
+    m = None
+    
+    def y(self, t):
+        return (1 - t)**3 * self.y0 + 3*(1 - t)**2 * t * self.y1 + 3*(1 - t) * t**2 * self.y2 + t**3  * self.y3
+    
+    def z(self, t):
+        return (1 - t)**3 * self.z0 + 3*(1 - t)**2 * t * self.z1 + 3*(1 - t) * t**2 * self.z2 + t**3  * self.z3
+    
+    def dy(self, t):
+        return 3 * (1 - t)**2 * (self.y1 - self.y0) + 6 * (1 - t) * t * (self.y2 - self.y1) + 3 * t**2 * (self.y3 - self.y2)
+    
+    def dz(self, t):
+        return 3 * (1 - t)**2 * (self.z1 - self.z0) + 6 * (1 - t) * t * (self.z2 - self.z1) + 3 * t**2 * (self.z3 - self.z2)
+    
+    def __init__(self, y_0, z_0, y_1, z_1, y_2, z_2, y_3, z_3):
+        self.y0 = y_0
+        self.z0 = z_0
+        self.y1 = y_1
+        self.z1 = z_1
+        self.y2 = y_2
+        self.z2 = z_2
+        self.y3 = y_3
+        self.z3 = z_3
+
+        self.I = bez_wing_I(self, d_le = self.D_LE, d_h = self.D_H, d_te = self.D_TE)
+        self.m = bez_wing_mass(self, d_le = self.D_LE, d_h = self.D_H, d_te = self.D_TE)
+
+class TriWing():
+    y0 = None
+    z0 = None
+    y1 = None
+    z1 = None
+    y2 = None
+    z2 = None
+
+    D_LE = 0.001 #m
+    D_S = 0.0005 #m
+    D_TE = 0.0005
+    
+    #1d array of [Ixx, Iyy, Izz, Ixy, Ixz, Iyz]
+    I = None
+    #Wing mass
+    m = None
+
+    def y(self, t):
+        t_adj = np.heaviside(t - .5, .5)
+        return ((self.y1 - self.y0) * 2*t + self.y0) * (1 - t_adj) + t_adj * ((self.y2 - self.y1) * (2*t - 1) + self.y1)
+    
+    def z(self, t):
+        t_adj = np.heaviside(t - .5, .5)
+        return ((self.z1 - self.z0) * 2*t + self.z0) * (1 - t_adj) + t_adj * ((self.z2 - self.z1) * (2*t - 1) + self.z1)
+        
+    def dy(self, t):
+        t_adj = np.heaviside(t - .5, .5)
+        return (self.y1 - self.y0) * (1 - t_adj) + t_adj * (self.y2 - self.y1)
+    
+    def dz(self, t):
+        t_adj = np.heaviside(t - .5, .5)
+        return (self.z1 - self.z0) * (1 - t_adj) + t_adj * (self.z2 - self.z1)
+        
+    def __init__(self, y0, z0, y1, z1, y2, z2):
+        self.y0 = y0
+        self.z0 = z0
+        self.y1 = y1
+        self.z1 = z1
+        self.y2 = y2
+        self.z2 = z2
+
+        self.I = line_I(y0, 0, y1, 0, diameter=self.D_LE) + line_I(y0, z0, y1, z1, diameter=self.D_TE) + line_I(y1, z0, y1, z1, diameter=self.D_S) + film_I(self)
+        self.m = line_m(y0, 0, y1, 0, diameter=self.D_LE) + line_m(y0, z0, y1, z1, diameter=self.D_TE) + line_m(y1, z0, y1, z1, diameter=self.D_S) + film_m(self)
+        
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    tri_wing = TriWing(0, 0, .005, -.01, .01, -.005)
+    t = np.linspace(0, 1)
+    plt.plot(tri_wing.dy(t), tri_wing.dz(t), marker = "o")
+
+    print(tri_wing.I)
