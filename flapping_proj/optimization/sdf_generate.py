rename fix for smoke test setup on top level run. Bug still found in tensor calculation

flapping_proj/optimization/sdf_generate.py
@@ -13,11 +13,11 @@ root = tree.getroot()
 
 # Load the JSON configuration
 json_config_path = os.path.join(curr_path,'data','config.json')
-with open(json_config_path, 'r') as json_file:
-    config = json.load(json_file)
 inertia_key = ['ixx', 'ixy', 'ixz', 'iyy', 'iyz', 'izz']
 
 def generate_sdf(output_path:str=None, chord_cps:np.array= None, spar_cps:np.array= None, blade_area:np.array=None):
+    with open(json_config_path, 'r') as json_file:
+        config = json.load(json_file)
     link_names = config['link_names']
     existing_links = {link_name: False for link_name in link_names}
     existing_joints = {joint_name: False for joint_name in config['joint_names']}