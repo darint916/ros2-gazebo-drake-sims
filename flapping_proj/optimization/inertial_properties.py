Film calculations tested and succeeds in all tests No actual error was found in the center of mass calculation, the truth value was just calculated incorrectly. Film calculators pass all tests.

flapping_proj/optimization/inertial_properties.py
@@ -65,7 +65,6 @@ def curve_I(curve, diameter = D_TE, t_max = 1, density = 1854.55) -> np.array:
                   0,
                   0,
                   sc.integrate.quad(lambda t: (curve.y(t) * curve.z(t)) *dm_curve(t), 0, t_max)[0]])
-     
     return I
 
 #calculates the mass of a rod following a curve in the yz plane
@@ -106,9 +105,10 @@ def film_m(curve, rho = rho_mem) -> float:
 # a curve multiplied by the curve mass so it can be divided 
 # by the overall system mass to get a system center of mass 
 # [y, z]
+# technically this calculates the moment with respect to an axis
 def film_com(curve, rho = rho_mem) -> np.array: #value tested
     return np.array([-rho * sc.integrate.quad(lambda t: curve.z(t) * curve.y(t) * curve.dy(t), 0, 1)[0],
-                     -rho / 2 * sc.integrate.quad(lambda t: (curve.z(t)**2 * curve.dy(t)), 0, 1)[0]])
+                     -rho / 2 * sc.integrate.quad(lambda t: curve.z(t)**2 * curve.dy(t), 0, 1)[0]])
 
 def bez_wing_I(curve, mass, d_le = D_LE, d_h = D_H, d_te = D_TE, debug = False) -> np.array:
     #leading edge

flapping_proj/unit_tests/test_film_inertial_properties.py
@@ -161,7 +161,7 @@ class Tri3(FilmCase):
 class Para1(FilmCase):
     name = "Parabola through (0, 0), (1, -1), (2, 0)"
     truth_mass = rho_mem * (4/3)
-    truth_com = [1, -8/15]
+    truth_com = [1, -.4]
     truth_i = np.array([32/105+8/5,
                         32/105,
                         8/5,

flapping_proj/unit_tests/test_wing_classes.py
@@ -23,7 +23,7 @@ class TestTriangleInequalities(unittest.TestCase):
         result = check_triangle_inequalities(tri_wing.I[0], tri_wing.I[1], tri_wing.I[2])
         Message.debug(f"I_origin: {tri_wing.I_origin[0]}, {tri_wing.I_origin[1]}, {tri_wing.I_origin[2]}, {tri_wing.I_origin[3]}, {tri_wing.I_origin[4]}, {tri_wing.I_origin[5]}")
         tensor_truth = np.array([148.692908, 6.875943, 141.835885, 0, 0, -3.224729]) / 1000**3 #1000**3 represents conversion from g mm**2 to kg m**2
-        tensor_origin_truth = np.array([594.049086, 8.422495, 585.645511, 0, 0, 22.974025]) / 1000**3
+        tensor_origin_truth = np.array([594.049086, 8.422495, 585.645511, 0, 0, -22.974025]) / 1000**3
         Message.debug(f"Tensor Truth Triangle Inequality Check: {check_triangle_inequalities(tensor_truth[0], tensor_truth[1], tensor_truth[2])}")
 
         com_truth = np.array([47.845714, -2.824405]) /1000
@@ -35,6 +35,7 @@ class TestTriangleInequalities(unittest.TestCase):
         Message.debug(f"Calculated mass: {tri_wing.mass}")
 
         Message.debug(f"tensor_origin_truth: {tensor_origin_truth}")
+        Message.debug(f"Calculated Origin tensor {tri_wing.I_origin}")
         self.assertTrue(result, (f"Triangle Inequalities are not satisfied: {tri_wing.I[0]}, {tri_wing.I[1]}, {tri_wing.I[2]}"))
         self.assertTrue(tri_wing.mass > 0)
         self.assertTrue(tri_wing.I[0] > 0)