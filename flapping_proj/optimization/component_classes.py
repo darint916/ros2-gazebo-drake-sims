872e55cbeed516c61d29d14d84773cd4363fba17
BrightKnight <kyled2002@hotmail.com>
2024-07-05 17:49:55 -0400

    Formatted code and removed some small deprecitaed things

--- flapping_proj/optimization/component_classes.py
+++ flapping_proj/optimization/component_classes.py
@@ -5,20 +5,23 @@ from optimization.geometry_classes import Geometry
 from optimization.geometry_classes import *
 from utils.message import Message
 
+
 def check_triangle_inequalities(Ixx, Iyy, Izz):
     return (Ixx + Iyy >= Izz) and (Iyy + Izz >= Ixx) and (Izz + Ixx >= Iyy)
 
 # If triangle test on the primary moments of inertia fails
-# adds a very small percentage to the smallest moment of inertia. 
+# adds a very small percentage to the smallest moment of inertia.
 # If the test is barely failing, this will make it pass.
 # Increases the smallest moment by at least 0.001%
 # or at most 0.01%
+
+
 def inertia_modifier(inertia: np.array):
     max_increase = 10
     for i in range(max_increase):
         # The error for a triangle inequality primary moments of inertia
         # These values are strictly positive for a valid tensor
-        tri_error = np.array([inertia[0] + inertia[1] - inertia[2], 
+        tri_error = np.array([inertia[0] + inertia[1] - inertia[2],
                               inertia[1] + inertia[2] - inertia[0],
                               inertia[0] + inertia[2] - inertia[1]])
         if not check_triangle_inequalities(inertia[0], inertia[1], inertia[2]):
@@ -26,11 +29,14 @@ def inertia_modifier(inertia: np.array):
             inertia[min_inertia_index] *= 1 + 1e-5
         else:
             if i > 0:
-                Message.info(f"Tensor modified from calculated value. Loop ran {i} times")
+                Message.info(
+                    f"Tensor modified from calculated value. Loop ran {i} times")
             break
         if i == max_increase - 1:
-            Message.error(f"No valid primary moments of inertia. Final tensor values were {inertia}")
+            Message.error(
+                f"No valid primary moments of inertia. Final tensor values were {inertia}")
     return inertia
+
 
 class Component():
     '''
@@ -40,21 +46,27 @@ class Component():
     Component.com: the center of mass of the Component in the yz plane relative to the origin (m, m)
     Component.i: a 1x6 array representing [Ixx, Iyy, Izz, Ixy, Ixz, Iyz] about the center of mass
     '''
+
     def __init__(self, geometry: Geometry, mass: float, com: np.array, inertia: np.array) -> None:
         self.geo = geometry
         self.mass = mass
         self.com = com
         self.I = inertia
-        
+
+
 class Film(Component):
     def __init__(self, upper: Curve, lower: Curve, thickness: float, density: float) -> None:
         bounded_region = BoundedRegion(upper, lower)
-        super().__init__(bounded_region, bounded_region.area * thickness * density, bounded_region.coa, thickness * density * bounded_region.second_moment)
-    
+        super().__init__(bounded_region, bounded_region.area * thickness * density,
+                         bounded_region.coa, thickness * density * bounded_region.second_moment)
+
+
 class Sweep(Component):
     def __init__(self, path: Curve, profile: BoundedRegion, density: float) -> None:
-        super().__init__(path, path.length * profile.area * density, path.coa, path.second_moment * profile.area * density)
-        
+        super().__init__(path, path.length * profile.area * density,
+                         path.coa, path.second_moment * profile.area * density)
+
+
 class Rod(Sweep):
     def __init__(self, y0: float, z0: float, y1: float, z1: float, diameter: float, density: float) -> None:
         line = Line(y0, z0, y1, z1)
--- flapping_proj/optimization/wing_classes.py
+++ flapping_proj/optimization/wing_classes.py
@@ -6,53 +6,54 @@ import matplotlib.pyplot as plt
 from optimization.geometry_classes import *
 from optimization.component_classes import *
 
-RHO_CF = 1854.55 #kg/m**3
-RHO_PET = 1383.995 #kg/m**3
+RHO_CF = 1854.55  # kg/m**3
+RHO_PET = 1383.995  # kg/m**3
 
 
 # generic wing object
 # assumes all components are placed correctly in the yz plane
-# This is a structure with a single film supported by 
+# This is a structure with a single film supported by
 # a collection of rigid members
 class Wing():
     # leading_edge is a curve (must be a fully solid member in reality)
     # trailing edge is a curve (may not be a fully solid member)
-    # leading_edge(y) >= trailing_edge(y). 
+    # leading_edge(y) >= trailing_edge(y).
     # sweeps is an array of Sweep instances
     # sweeps must include the sweep forming the leading edge
-    def __init__(self, leading_edge: Curve, trailing_edge: Curve, sweeps = []):
-        self.cached_mass = None
-        self.cached_com = None
-        self.I = None
-        self.cached_I_origin = None
-        
+    def __init__(self, leading_edge: Curve, trailing_edge: Curve, sweeps=[]):
         self.components = [Film(leading_edge, trailing_edge, 12e-6, RHO_PET)]
         for i in sweeps:
             self.components.append(i)
-            
+
         self.mass = 0
         for i in self.components:
             self.mass += i.mass
-            
+
         self.com = np.zeros(2)
         for i in self.components:
             self.com += i.com * i.mass
         self.com /= self.mass
-        
+
         self.I = np.zeros(6)
         for i in self.components:
             del_y = i.com[0] - self.com[0]
             del_z = i.com[1] - self.com[1]
             displacement_radius = (del_y)**2 + (del_z)**2
-            self.I += i.I + i.mass * np.array([displacement_radius, displacement_radius - del_y**2, displacement_radius - del_z, 0, 0, -del_y * del_z])
-        
-        self.I = inertia_modifier(self.I)
-        
-        com_magnitude_sqr = self.com[0]**2 + self.com[1]**2
-        self.I_origin = self.I + self.mass * np.array([com_magnitude_sqr, com_magnitude_sqr - self.com[0]**2, com_magnitude_sqr - self.com[1], 0, 0, -self.com[0]*self.com[1]])
+            self.I += i.I + i.mass * \
+                np.array([displacement_radius, displacement_radius - del_y **
+                         2, displacement_radius - del_z, 0, 0, -del_y * del_z])
 
+        self.I = inertia_modifier(self.I)
+
+        com_magnitude_sqr = self.com[0]**2 + self.com[1]**2
+        self.I_origin = self.I + self.mass * \
+            np.array([com_magnitude_sqr, com_magnitude_sqr - self.com[0]**2,
+                     com_magnitude_sqr - self.com[1], 0, 0, -self.com[0]*self.com[1]])
+
+# depriciated
 class BezierWing():
     def __init__(self, y_0, z_0, y_1, z_1, y_2, z_2, y_3, z_3):
+        print("WARNING: BezierWing uses depriciated components and is not yet updated to Component internal format.")
         self.y0 = y_0
         self.z0 = z_0
         self.y1 = y_1
@@ -62,30 +63,34 @@ class BezierWing():
         self.y3 = y_3
         self.z3 = z_3
 
-        self.m = bez_wing_mass(self, d_le = self.D_LE, d_h = self.D_H, d_te = self.D_TE)
+        self.m = bez_wing_mass(self, d_le=self.D_LE,
+                               d_h=self.D_H, d_te=self.D_TE)
 
-        self.I = bez_wing_I(self, self.m, d_le = self.D_LE, d_h = self.D_H, d_te = self.D_TE)
-        
+        self.I = bez_wing_I(self, self.m, d_le=self.D_LE,
+                            d_h=self.D_H, d_te=self.D_TE)
+
     def y(self, t):
-        return (1 - t)**3 * self.y0 + 3*(1 - t)**2 * t * self.y1 + 3*(1 - t) * t**2 * self.y2 + t**3  * self.y3
-    
+        return (1 - t)**3 * self.y0 + 3*(1 - t)**2 * t * self.y1 + 3*(1 - t) * t**2 * self.y2 + t**3 * self.y3
+
     def dy(self, t):
         return 3 * (1 - t)**2 * (self.y1 - self.y0) + 6 * (1 - t) * t * (self.y2 - self.y1) + 3 * t**2 * (self.y3 - self.y2)
-    
+
     def lower_z(self, t):
-        return (1 - t)**3 * self.z0 + 3*(1 - t)**2 * t * self.z1 + 3*(1 - t) * t**2 * self.z2 + t**3  * self.z3
-    
+        return (1 - t)**3 * self.z0 + 3*(1 - t)**2 * t * self.z1 + 3*(1 - t) * t**2 * self.z2 + t**3 * self.z3
+
     def lower_dz(self, t):
         return 3 * (1 - t)**2 * (self.z1 - self.z0) + 6 * (1 - t) * t * (self.z2 - self.z1) + 3 * t**2 * (self.z3 - self.z2)
-    
+
     def upper_z(self, t):
         return 0
-    
+
     def upper_dz(self, t):
         return 0
 
-#Inertial Return: 1d array of [Ixx, Iyy, Izz, Ixy, Ixz, Iyz]
-class TriWing(Wing):    
+# Inertial Return: 1d array of [Ixx, Iyy, Izz, Ixy, Ixz, Iyz]
+
+
+class TriWing(Wing):
     def __init__(self, y0, z0, y1, z1, y2, z2):
         self.y0 = np.round(y0, 12)
         self.z0 = np.round(z0, 12)
@@ -94,22 +99,28 @@ class TriWing(Wing):
         self.y2 = np.round(y2, 12)
         self.z2 = np.round(z2, 12)
 
-        self.leading_edge_rod_diameter = 0.001 #m leading edge carbon fiber rod diameter
-        self.spar_rod_diameter = 0.0005 #m Spar carbon fiber rod diameter
-        self.trailing_edge_rod_diameter = 0.0005 #m trailing edge carbon fiber rod diameter
-        
+        # m leading edge carbon fiber rod diameter
+        self.leading_edge_rod_diameter = 0.001
+        self.spar_rod_diameter = 0.0005  # m Spar carbon fiber rod diameter
+        # m trailing edge carbon fiber rod diameter
+        self.trailing_edge_rod_diameter = 0.0005
+
         leading_edge = Line(self.y0, 0, self.y2, 0)
-        trailing_edge = LineSegments([self.y0, self.y1, self.y2], [self.z0, self.z1, self.z2])
+        trailing_edge = LineSegments([self.y0, self.y1, self.y2], [
+                                     self.z0, self.z1, self.z2])
         rods = [Rod(0.002, 0, self.y2, 0, self.leading_edge_rod_diameter, RHO_CF),
-                Rod(self.y0, self.z0, self.y1, self.z1, self.trailing_edge_rod_diameter, RHO_CF),
+                Rod(self.y0, self.z0, self.y1, self.z1,
+                    self.trailing_edge_rod_diameter, RHO_CF),
                 Rod(self.y1, self.z0, self.y1, self.z1, self.spar_rod_diameter, RHO_CF)]
         super().__init__(leading_edge, trailing_edge, rods)
-    
+
+
 class StraightButterflyWing(Wing):
     def __init__(self, film_start: float, leading_edge_length: float, leading_edge_angle: float, mid_bar_length: float, mid_bar_angle: float, trailing_edge_length: float, trailing_edge_angle: float):
         raise Exception("Not yet supported")
         super().__init__(leading_edge, trailing_edge, sweeps)
-    
+
+
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
@@ -120,7 +131,7 @@ if __name__ == "__main__":
     t = np.linspace(0, 1)
     # plt.plot(tri_wing.y(t), tri_wing.z(t))
     # plt.plot(tri_wing.y(t), tri_wing.dz(t) / tri_wing.dy(t), marker = "o")
-    
+
     print(tri_wing.mass)
     print(tri_wing.com)
     print(tri_wing.I)
