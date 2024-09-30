7623fc4f63fe70ce44ffde3a7f35f6b4ce03d5a7
Darin Tsai <darint916@gmail.com>
2024-09-30 13:39:04 -0400

    inertial fixes, can render (need to fix aero blades)

--- flapping_proj/optimization/generate_param_script.py
+++ flapping_proj/optimization/generate_param_script.py
@@ -1,13 +1,13 @@
 import optimization.wing_classes as wing_classes
 import numpy as np
 from optimization.aero_properties import aero_properties
-total_rotate = np.array([[0, 1, 0], [0, 0, -1], [-1, 0, 0]])
+total_rotate = np.array([[0, 1, 0], [0, 0, 1], [1, 0, 0]])
 print("Total Rotate:\n", total_rotate)
-x_rot = np.array([[1, 0, 0], [0, 0, -1], [0, 1, 0]])
-y_rot = np.array([[0, 0, 1], [0, 1, 0], [-1, 0, 0]])
-z_rot = np.array([[0, 1, 0], [-1, 0, 0], [0, 0, 1]])
-print(z_rot @ y_rot)
-print("rot test d")
+# x_rot = np.array([[1, 0, 0], [0, 0, -1], [0, 1, 0]])
+# y_rot = np.array([[0, 0, 1], [0, 1, 0], [-1, 0, 0]])
+# z_rot = np.array([[0, 1, 0], [-1, 0, 0], [0, 0, 1]])
+# print(z_rot @ y_rot)
+# print("rot test d")
 test_wing = wing_classes.ThreeSegmentWing(
 0.1,#     wing_length: float,
 0.01,#     root_edge: float,
@@ -17,8 +17,8 @@ test_wing = wing_classes.ThreeSegmentWing(
 40 / 180 * np.pi,#     spar_angle_1: float,
 0.03#     spar_length_1: float
 )
-print("Inertia (leading edge on y axis, wing on yz): ixx iyy izz ixy ixz iyz", test_wing.I)
-print("Inerita change to wing on xy plane: ixx iyy izz ixy ixz iyz", total_rotate @ np.array([[test_wing.I[0], test_wing.I[3], test_wing.I[4]], [test_wing.I[3], test_wing.I[1], test_wing.I[5]], [test_wing.I[4], test_wing.I[5], test_wing.I[2]]]) @ total_rotate.T)
+print("Inertia (leading edge on y axis, wing on -yz): ixx iyy izz ixy ixz iyz", test_wing.I)
+print("Inerita change to wing on xy plane: ixx iyy izz ixy ixz iyz\n", total_rotate @ np.array([[test_wing.I[0], test_wing.I[3], test_wing.I[4]], [test_wing.I[3], test_wing.I[1], test_wing.I[5]], [test_wing.I[4], test_wing.I[5], test_wing.I[2]]]) @ total_rotate.T)
 print("Mass: ", test_wing.mass)
 print("COM: ", test_wing.com)
 chord_cp, spar_cp, blade_areas = aero_properties(
